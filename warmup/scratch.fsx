41d0843a3bc065ea8423ddfd5270bb4d92431657
Daniel Markham <danielbmarkham@hotmail.com>
2022-11-26 19:16:44 -0500

    more on later examples

--- warmup/scratch.fsx
+++ warmup/scratch.fsx
@@ -1,3 +1,5 @@
+// Composable Lenses On The Data
+
 let getRecordsFrom fileName = System.IO.File.ReadLines(fileName)   |> Seq.toList |>List.map(fun x->x.Split [|'\t'|]) |> List.filter(fun x->x.Length>=5);;
 let allRecords = getRecordsFrom "warmup/example.tsv";;
 let allTypes (incomingRecords:string[] list) : string[] list = incomingRecords|>List.filter(fun x->x[0].Length>0)
@@ -37,6 +39,23 @@ let valuesMatching (sMatchingPattern:string) (incomingRecords:string[] list): st
 let eventTimesMatching (sMatchingPattern:string) (incomingRecords:string[] list): string[] list=
   let matchRegex=System.Text.RegularExpressions.Regex(sMatchingPattern)
   incomingRecords|>allEventTimes|>List.filter(fun x->matchRegex.IsMatch x[4])
+let typeFirstChanged(incomingRecords:string[] list):string[]  =
+  incomingRecords |> allTypes |> allEventTimes |> List.last
+let typeLastChanged(incomingRecords:string[] list):string[]  =
+  incomingRecords |> allTypes |> allEventTimes |> List.head
+let entityFirstChanged(incomingRecords:string[] list):string[]  =
+  incomingRecords |> allEntities |> allEventTimes |> List.last
+let entityLastChanged(incomingRecords:string[] list):string[]  =
+  incomingRecords |> allEntities |> allEventTimes |> List.head
+let attributeFirstChanged (incomingRecords:string[] list):string[]  =
+  incomingRecords |> allAttributes |> allEventTimes |> List.last
+let attributeLastChanged (incomingRecords:string[] list):string[]  =
+  incomingRecords |> allAttributes |> allEventTimes |> List.head
+let valueFirstChanged(incomingRecords:string[] list):string[]  =
+  incomingRecords |> allValues |> allEventTimes |> List.last
+let valueLastChanged(incomingRecords:string[] list):string[]  =
+  incomingRecords |> allValues |> allEventTimes |> List.head
+
 
 type PrettyPrintOptions = |ByType=0|ByInstance=1|ByAttribute=2|ByValue=3
 let prettyPrint (opt:PrettyPrintOptions) (incomingRecords:string[] list) =
@@ -47,3 +66,38 @@ let prettyPrint (opt:PrettyPrintOptions) (incomingRecords:string[] list) =
 // EAV or EAV/CR
 // Decided that Type Instance Attribute Value datetime was all that was required
 // It's a logging question!
+
+// Tools to make sample data
+
+let pickRandomListItem (arr:List<'a>) =
+  let r=new System.Random()
+  let choice=r.Next(arr.Length)
+  arr.Item(choice)
+let pickRandomLightState()=pickRandomListItem(["Light Off";"Light On"])
+let pickRandomLight4()=pickRandomListItem(["Simpson's House"; "Simpson's Barn"; "Home Base"; "Happy Gorilla Pub"; "Joe's Hospital"; "City Hall"])
+let pickADateTimeBetweenTwoDateTimes() (sDateFrom:string) (sDateTo:string):System.DateTime =
+  try
+    let r=new System.Random()
+    let dtFrom=System.DateTime.Parse(sDateFrom)
+    let dtTo=System.DateTime.Parse(sDateTo)
+    let range=int (dtTo-dtFrom).TotalMinutes
+    dtFrom.AddMinutes(r.Next(range))
+  with _->System.DateTime.MinValue
+let getRandomLightRecord4() sFrom sTo =
+  pickRandomLight4() + "\t" + pickRandomLightState() + "\t" + (pickADateTimeBetweenTwoDateTimes() sFrom sTo).ToString("g")
+
+let randomizedLogProblem4 sFrom sTo =Seq.initInfinite(fun index->getRandomLightRecord4() sFrom sTo)
+
+let differentInstances5=["Simpson's House"; "Simpson's Barn"; "Home Base"; "Happy Gorilla Pub"; "Joe's Hospital"; "City Hall"; "Old Beaver Dam"; "Main Street"; "Elm Street Railroad Crossing"; "Oscar Jones Home"; "Oscar Jones Barn"; "Oscar Jones Front Yard"; "Bellweather Inn"; "SnagglePus Hotel";"Remants Of The Day Consignment Shop"; "Moe's Mowers"; "Grinch's Grocery"; "City Jail";"Horace Horse";"Plum Ferry Service"]
+let pickRandomInstance5()=pickRandomListItem(differentInstances5)
+let differentEmployees5=["Joe Bob"; "Tom Fool" ;"Billy Bob"; "Roberta"; "Billy Sue"; "Odus"; "Miss Anthrope"; "Bigus Dickus"; "Sosue Mi"; "Nachos Normaldata"; "Hepmi"; "Gno Problem"; "Jeeves Louise"; "Orca"]
+let pickRandomEmployee5()=pickRandomListItem(differentEmployees5)
+let differentTypes5=["Light Bulb"; "Electric Banana"]
+let pickRandomType5()=pickRandomListItem(differentTypes5)
+let differentValues5=["On"; "Off"]
+let pickRandomValue5()=pickRandomListItem(differentValues5)
+let pickRandomRecord5() sFrom sTo =
+  pickRandomType5() + "\t" + pickRandomInstance5() + "\t" + "State" + "\t" + pickRandomValue5() + "\t" + (pickADateTimeBetweenTwoDateTimes() sFrom sTo).ToString("g")
+let randomizedLogProblem5 sFrom sTo =Seq.initInfinite(fun index->pickRandomRecord5() sFrom sTo)
+
+//System.IO.File.WriteAllLines("warmup/example4.tsv", foo |> Seq.take 300)
