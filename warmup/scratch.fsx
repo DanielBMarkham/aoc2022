690b26a8dd4e752ab939e3ca6c365ce93683287b
Daniel Markham <danielbmarkham@hotmail.com>
2022-11-30 20:30:49 -0500

    added some unicode playing card lookups to increase loc

--- warmup/scratch.fsx
+++ warmup/scratch.fsx
@@ -122,9 +122,78 @@ let CardSuits =[Hearts;Diamonds;Spades;Clubs]
 type CardRank = |Ace|Two|Three|Four|Five|Six|Seven|Eight|Nine|Ten|Jack|Queen|King
 let CardRanks= [Ace;Two;Three;Four;Five;Six;Seven;Eight;Nine;Ten;Jack;Queen;King]
 type PlayingCard = {Suit:CardSuit; Rank:CardRank}
-let playingDeck=CardRanks|> List.collect (fun x -> CardSuits |> List.map (fun y-> x, y));;
+let unicodeCards:(string*CardSuit*CardRank) list =
+  [
+  ("\U0001F0A1", Clubs, Ace);
+  ("\U0001F0A2", Clubs, Two);
+  ("\U0001F0A3", Clubs, Three);
+  ("\U0001F0A4", Clubs, Four);
+  ("\U0001F0A5", Clubs, Five);
+  ("\U0001F0A6", Clubs, Six);
+  ("\U0001F0A7", Clubs, Seven);
+  ("\U0001F0A8", Clubs, Eight);
+  ("\U0001F0A9", Clubs, Nine);
+  ("\U0001F0AA", Clubs, Ten);
+  ("\U0001F0AB", Clubs, Jack);
+//  ("\U0001F0AC", Clubs, Knight);
+  ("\U0001F0AD", Clubs, Queen);
+  ("\U0001F0AE", Clubs, King);
+//"\U0001F0A0" Back of card
+  ("\U0001F0B1", Hearts, Ace);
+  ("\U0001F0B2", Hearts, Two);
+  ("\U0001F0B3", Hearts, Three);
+  ("\U0001F0B4", Hearts, Four);
+  ("\U0001F0B5", Hearts, Five);
+  ("\U0001F0B6", Hearts, Six);
+  ("\U0001F0B7", Hearts, Seven);
+  ("\U0001F0B8", Hearts, Eight);
+  ("\U0001F0B9", Hearts, Nine);
+  ("\U0001F0BA", Hearts, Ten);
+  ("\U0001F0BB", Hearts, Jack);
+//  ("\U0001F0BC", Hearts, Knight);
+  ("\U0001F0BD", Hearts, Queen);
+  ("\U0001F0BE", Hearts, King);
+// "\U0001F0BF", Hearts, Joker);
+  ("\U0001F0C1", Diamonds, Ace);
+  ("\U0001F0C2", Diamonds, Two);
+  ("\U0001F0C3", Diamonds, Three);
+  ("\U0001F0C4", Diamonds, Four);
+  ("\U0001F0C5", Diamonds, Five);
+  ("\U0001F0C6", Diamonds, Six);
+  ("\U0001F0C7", Diamonds, Seven);
+  ("\U0001F0C8", Diamonds, Eight);
+  ("\U0001F0C9", Diamonds, Nine);
+  ("\U0001F0CA", Diamonds, Ten);
+  ("\U0001F0CB", Diamonds, Jack);
+//  ("\U0001F0CC", Diamonds, Knight);
+  ("\U0001F0CD", Diamonds, Queen);
+  ("\U0001F0CE", Diamonds, King);
+//"\U0001F0CF", Red, Joker);
+  ("\U0001F0D1", Clubs, Ace);
+  ("\U0001F0D2", Clubs, Two);
+  ("\U0001F0D3", Clubs, Three);
+  ("\U0001F0D4", Clubs, Four);
+  ("\U0001F0D5", Clubs, Five);
+  ("\U0001F0D6", Clubs, Six);
+  ("\U0001F0D7", Clubs, Seven);
+  ("\U0001F0D8", Clubs, Eight);
+  ("\U0001F0D9", Clubs, Nine);
+  ("\U0001F0DA", Clubs, Ten);
+  ("\U0001F0DB", Clubs, Jack);
+//  ("\U0001F0DC", Clubs, Knight);
+  ("\U0001F0DD", Clubs, Queen);
+  ("\U0001F0DE", Clubs, King);
+//"\U0001F0DF", White, Trump);
+  ]
 
+let playingDeck: (CardSuit * CardRank) list=CardSuits|> List.collect (fun x -> CardRanks |> List.map (fun y-> x, y));;
 let deal numHands handSize cards =
   let rnd=System.Random()
   let shuffledCards=cards |> List.sortBy(fun _ ->rnd.Next(1,52))
   ((shuffledCards|>List.chunkBySize handSize |> Seq.take numHands), (shuffledCards|>Seq.skip (numHands*handSize)))
+
+let lookupCardUnicode (card:(CardSuit*CardRank)):string =
+  let ret= unicodeCards|>List.tryFind(fun (char,suit,rank)->fst card=suit && snd card=rank)
+  match ret with 
+    | Some (a,b,c)->a 
+    |_None->""
