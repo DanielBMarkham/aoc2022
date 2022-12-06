39315ab512541623626f50da82ad17d8857c61f7
Daniel Markham <danielbmarkham@hotmail.com>
2022-12-06 11:01:47 -0500

    playing

--- day6/scratch.fsx
+++ day6/scratch.fsx
@@ -11,3 +11,8 @@ let progressivelyBiggerChunksOfString (s:string) =
 let prob1 x=x|>progressivelyBiggerChunksOfString|>Array.mapi(fun i x->(i,x,lastNCharactersDifferent 4 x))|>Array.find(fun (a,b,c)->c)
 
 let prob2 x=x|>progressivelyBiggerChunksOfString|>Array.mapi(fun i x->(i,x,lastNCharactersDifferent 14 x))|>Array.find(fun (a,b,c)->c)
+
+
+// One liner using windowed views
+let findFirstDistinct (windowSize:int) (s:string) = (s.ToCharArray()|>Array.windowed windowSize |>Array.find(fun x->x|>Array.distinct|>Array.length=windowSize)|>System.String|>s.IndexOf) + windowSize
+linesExample|>List.map(fun x->findFirstDistinct 4 x)
