17cb5bbd38d13078eea4daa39addcb607b62d706
Daniel Markham <danielbmarkham@hotmail.com>
2022-12-08 02:22:32 -0500

    Day 8

---
+++ day8/scratch.fsx
@@ -0,0 +1,14 @@
+
+let linesExample = System.IO.File.ReadLines("day8/example.txt")   |> Seq.toList;;
+let linesProblem1 = System.IO.File.ReadLines("day8/problem1.txt")   |> Seq.toList;;
+let linesProblem2 = System.IO.File.ReadLines("day8/problem2.txt")   |> Seq.toList;;
+
+let splitBy f input =
+  let i = ref 0
+  input
+  |> Seq.groupBy (fun x ->
+    //if f x then incr i
+    if f x then (i.Value<-i.Value+1)
+    i.Value)
+  |> Seq.map snd
+
